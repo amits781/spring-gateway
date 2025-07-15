55ba7044897e9caf267fde3caacd9e3992f6d0d3
Amit Sharma <amits781@gmail.com>
2025-07-15 15:03:12 +0530

    added gateway

---
+++ aidyn-app-gateway/src/test/java/com/aidyn/gateway/gateway/AidynAppGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidyn.gateway.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidynAppGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
